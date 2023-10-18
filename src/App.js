3b24f80be479d2a9407e4ad5ef498e77e09b6026
Khushboo Chouhan <chouhankhushboo59@gmail.com>
2023-10-18 15:42:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,30 +5,15 @@ import Aboutus from './Components/AboutUs';
 
 
 function App() {
-  const name="khushboo"
-  
+  // const name="khushboo"
+// const [name,setname]=useState()
+// const [state, setState] = useState(initialState);
   return (
     <div>
-      {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header> */}
-      <>
-      <h4>name:{name}</h4>
-      <Home1></Home1>
+      {/* <h4>name:{name}</h4> */}
+      <Home1 name="khushboo"></Home1>
       <Aboutus/>
-      </>
-    </div>
+      </div>
   );
 }
 
